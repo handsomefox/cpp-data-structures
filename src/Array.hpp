448db1e0e35cceb7432195737c777d6bc617f5f1
handsomefox <handsomefox.hf@gmail.com>
2021-04-10 20:24:36 +0300

    Array: Remove #include <iostream> and delete set_null() + other things.

--- src/Array.hpp
+++ src/Array.hpp
@@ -1,20 +1,14 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
-#include <iostream>
 namespace mango {
 template <typename T, size_t N> class Array {
 public:
   Array() = default;
 
-  constexpr bool empty() noexcept { return N == 0 ? true : false; }
-
-  constexpr void set_null() noexcept { memset(data_, 0, N * sizeof(T)); }
-
+  constexpr bool empty() { return N == 0; }
   constexpr size_t size() noexcept { return N; }
-
   constexpr T *data() noexcept { return data_; }
-  constexpr const T *data() const noexcept { return data_; }
-
+  [[nodiscard]] constexpr const T *data() const noexcept { return data_; }
   constexpr const T &operator[](const size_t &index) const noexcept {
     return data_[index];
   }
