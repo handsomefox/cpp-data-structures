4df77b39ea40bcfdc1b970c733d2021224421ac6
handsomefox <handsomefox.hf@gmail.com>
2021-04-24 17:59:48 +0300

    Add iterators

--- src/Array.hpp
+++ src/Array.hpp
@@ -1,21 +1,67 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
-namespace mango {
-template <typename T, size_t N> class Array {
-public:
-  Array() = default;
+#include <iostream>
 
-  constexpr bool empty() { return N == 0; }
-  constexpr size_t size() noexcept { return N; }
-  constexpr T *data() noexcept { return data_; }
-  [[nodiscard]] constexpr const T *data() const noexcept { return data_; }
-  constexpr const T &operator[](const size_t &index) const noexcept {
-    return data_[index];
-  }
-  constexpr T &operator[](const size_t &index) noexcept { return data_[index]; }
+namespace mango
+{
+	template <typename Array>
+	struct ArrIter
+	{
+		using iterator_category = std::forward_iterator_tag;
+		using value_type = Array;
+		using difference_type = std::ptrdiff_t;
+		using pointer = Array*;
+		using reference = Array&;
 
-private:
-  T data_[N];
-};
+		ArrIter(pointer ptr) : m_ptr(ptr) {}
+
+		reference operator*() const { return *m_ptr; }
+		pointer operator->() { return m_ptr; }
+
+		ArrIter& operator++()
+		{
+			++m_ptr;
+			return *this;
+		}
+		ArrIter operator++(Array)
+		{
+			ArrIter tmp = *this;
+			++(*this);
+			return tmp;
+		}
+
+		friend bool operator==(const ArrIter& a, const ArrIter& b) { return a.m_ptr == b.m_ptr; }
+		friend bool operator!=(const ArrIter& a, const ArrIter& b) { return a.m_ptr != b.m_ptr; }
+
+	private:
+		pointer m_ptr = nullptr;
+	};
+	template <typename T, size_t N>
+	class Array
+	{
+	public:
+		Array() = default;
+
+		constexpr bool empty() { return N == 0; }
+		constexpr size_t size() noexcept { return N; }
+		constexpr T* data() noexcept { return data_; }
+		[[nodiscard]] constexpr const T* data() const noexcept { return data_; }
+		constexpr const T& operator[](const size_t& index) const noexcept
+		{
+			return data_[index];
+		}
+		constexpr T& operator[](const size_t& index) noexcept { return data_[index]; }
+		ArrIter<T> begin()
+		{
+			return ArrIter<T>(&data_[0]);
+		}
+		ArrIter<T> end()
+		{
+			return ArrIter<T>(&data_[N]);
+		}
+
+	private:
+		T data_[N];
+	};
 } // namespace mango
 #endif // ARRAY_HPP
--- src/Vector.hpp
+++ src/Vector.hpp
@@ -2,138 +2,196 @@
 #define VECTOR_HPP
 #include <iostream>
 
-namespace mango {
-template <typename T> class Vector {
-public:
-  Vector() { reserve(2); }
+namespace mango
+{
+	template <typename Vector>
+	struct VecIter
+	{
+		using iterator_category = std::forward_iterator_tag;
+		using value_type = Vector;
+		using difference_type = std::ptrdiff_t;
+		using pointer = Vector*;
+		using reference = Vector&;
 
-  explicit Vector(const size_t capacity) { reserve(capacity); }
+		VecIter(pointer ptr) : m_ptr(ptr) {}
 
-  Vector(const Vector &vec) {
-    size_ = vec.size_;
-    capacity_ = vec.capacity_;
+		reference operator*() const { return *m_ptr; }
+		pointer operator->() { return m_ptr; }
 
-    data_ = new T[sizeof &vec.data_];
-    memcpy_s(data_, sizeof data_, vec.data_, sizeof vec.data_);
-  }
+		VecIter& operator++()
+		{
+			++m_ptr;
+			return *this;
+		}
+		VecIter operator++(Vector)
+		{
+			VecIter tmp = *this;
+			++(*this);
+			return tmp;
+		}
 
-  Vector(Vector &&vec) noexcept {
-    size_ = vec.size_;
-    capacity_ = vec.capacity_;
-    data_ = std::move(vec.data_);
-  }
+		friend bool operator==(const VecIter& a, const VecIter& b) { return a.m_ptr == b.m_ptr; }
+		friend bool operator!=(const VecIter& a, const VecIter& b) { return a.m_ptr != b.m_ptr; }
 
-  ~Vector() { delete[] data_; }
+	private:
+		pointer m_ptr = nullptr;
+	};
 
-  T &front() { return data_[0]; }
+	template <typename T>
+	class Vector
+	{
+	public:
+		Vector() { reserve(2); }
 
-  T &back() { return data_[size_ - 1]; }
+		explicit Vector(const size_t capacity) { reserve(capacity); }
 
-  T *data() { return data_; }
+		Vector(const Vector& vec)
+		{
+			size_ = vec.size_;
+			capacity_ = vec.capacity_;
 
-  void reserve(const size_t new_capacity) {
-    capacity_ += new_capacity;
-    T *newData = new T[capacity_];
+			data_ = new T[sizeof & vec.data_];
+			memcpy_s(data_, sizeof data_, vec.data_, sizeof vec.data_);
+		}
 
-    for (size_t i = 0; i < size_; ++i)
-      newData[i] = data_[i];
+		Vector(Vector&& vec) noexcept
+		{
+			size_ = vec.size_;
+			capacity_ = vec.capacity_;
+			data_ = std::move(vec.data_);
+		}
 
-    delete[] data_;
-    data_ = newData;
-  }
+		~Vector() { delete[] data_; }
 
-  [[nodiscard]] bool empty() const { return size_ == 0 ? true : false; }
+		T& front() { return data_[0]; }
 
-  size_t size() { return size_; }
-  [[nodiscard]] size_t size() const { return size_; }
+		T& back() { return data_[size_ - 1]; }
 
-  size_t capacity() { return capacity_; }
-  [[nodiscard]] size_t capacity() const { return capacity_; }
+		T* data() { return data_; }
 
-  void clear() {
-    for (size_t i = 0; i < size_; ++i)
-      data_[i].~T();
-    size_ = 0;
-  }
+		void reserve(const size_t new_capacity)
+		{
+			capacity_ += new_capacity;
+			T* newData = new T[capacity_];
 
-  void push_back(T data) {
-    if (size_ >= capacity_)
-      reserve(capacity_ + capacity_ / 2);
+			for (size_t i = 0; i < size_; ++i)
+				newData[i] = data_[i];
 
-    data_[size_++] = data;
-  }
+			delete[] data_;
+			data_ = newData;
+		}
 
-  template <typename... Args> T &emplace_back(Args &&...args) {
-    if (size_ >= capacity_)
-      reserve(capacity_ + capacity_ / 2);
+		[[nodiscard]] bool empty() const { return size_ == 0 ? true : false; }
 
-    data_[size_] = T(std::forward<Args>(args)...);
-    return data_[size_++];
-  }
+		size_t size() { return size_; }
+		[[nodiscard]] size_t size() const { return size_; }
 
-  void pop_back() {
-    if (size_ > 0) {
-      size_--;
-      data_[size_].~T();
-    }
-  }
+		size_t capacity() { return capacity_; }
+		[[nodiscard]] size_t capacity() const { return capacity_; }
 
-  void resize(const size_t size) {
-    T *newData = new T[size];
+		void clear()
+		{
+			for (size_t i = 0; i < size_; ++i)
+				data_[i].~T();
+			size_ = 0;
+		}
 
-    for (size_t i = 0; i < size; ++i) {
-      if (i < size_)
-        newData[i] = data_[i];
-      else if (std::is_integral_v<T>)
-        newData[i] = static_cast<T>(0);
-    }
-    delete[] data_;
+		void push_back(T data)
+		{
+			if (size_ >= capacity_)
+				reserve(capacity_ + capacity_ / 2);
 
-    size_ = size;
-    capacity_ = size;
-    data_ = newData;
-  }
+			data_[size_++] = data;
+		}
 
-  void print() {
-    std::cout << "Printing Vector: \n";
-    for (size_t i = 0; i < size_; ++i)
-      std::cout << data_[i] << "\n";
-  }
+		template <typename... Args>
+		T& emplace_back(Args &&...args)
+		{
+			if (size_ >= capacity_)
+				reserve(capacity_ + capacity_ / 2);
 
-  const T &operator[](size_t index) const { return data_[index]; }
+			data_[size_] = T(std::forward<Args>(args)...);
+			return data_[size_++];
+		}
 
-  constexpr T &operator[](size_t index) { return data_[index]; }
+		void pop_back()
+		{
+			if (size_ > 0)
+			{
+				size_--;
+				data_[size_].~T();
+			}
+		}
 
-  constexpr Vector &operator=(const Vector &vec) {
-    if (this == &vec)
-      return *this;
+		void resize(const size_t size)
+		{
+			T* newData = new T[size];
 
-    this->size_ = vec.size_;
-    this->capacity_ = vec.capacity_;
+			for (size_t i = 0; i < size; ++i)
+			{
+				if (i < size_)
+					newData[i] = data_[i];
+				else if (std::is_integral_v<T>)
+					newData[i] = static_cast<T>(0);
+			}
+			delete[] data_;
 
-    data_ = new T[sizeof &vec.data_];
-    memcpy_s(data_, sizeof data_, vec.data_, sizeof vec.data_);
+			size_ = size;
+			capacity_ = size;
+			data_ = newData;
+		}
 
-    return *this;
-  }
+		void print()
+		{
+			std::cout << "Printing Vector: \n";
+			for (size_t i = 0; i < size_; ++i)
+				std::cout << data_[i] << "\n";
+		}
 
-  constexpr Vector &operator=(Vector &&vec) noexcept {
-    if (this == &vec)
-      return *this;
+		const T& operator[](size_t index) const { return data_[index]; }
 
-    size_ = vec.size_;
-    capacity_ = vec.capacity_;
+		constexpr T& operator[](size_t index) { return data_[index]; }
 
-    data_ = vec.data_;
+		constexpr Vector& operator=(const Vector& vec)
+		{
+			if (this == &vec)
+				return *this;
 
-    return *this;
-  }
+			this->size_ = vec.size_;
+			this->capacity_ = vec.capacity_;
 
-private:
-  T *data_ = nullptr;
+			data_ = new T[sizeof & vec.data_];
+			memcpy_s(data_, sizeof data_, vec.data_, sizeof vec.data_);
 
-  size_t size_ = 0;
-  size_t capacity_ = 0;
-};
+			return *this;
+		}
+
+		constexpr Vector& operator=(Vector&& vec) noexcept
+		{
+			if (this == &vec)
+				return *this;
+
+			size_ = vec.size_;
+			capacity_ = vec.capacity_;
+
+			data_ = vec.data_;
+
+			return *this;
+		}
+		VecIter<T> begin()
+		{
+			return VecIter<T>(&data_[0]);
+		}
+		VecIter<T> end()
+		{
+			return VecIter<T>(&data_[size_]);
+		}
+
+	private:
+		T* data_ = nullptr;
+
+		size_t size_ = 0;
+		size_t capacity_ = 0;
+	};
 } // namespace mango
 #endif // VECTOR_HPP
